fa45e6d75b20167cddb7b16bfaf2be488a09d048
Jiho Rhee <rhee519@gmail.com>
2021-07-07 14:03:12 +0900

    210706 todo list

---
+++ js/greeting.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+}
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings();
+}
---
+++ js/todo.js
@@ -0,0 +1,21 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    todoList.appendChild(li);
+}
+
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
